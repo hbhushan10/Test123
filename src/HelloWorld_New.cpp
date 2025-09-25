d2b1e6a7137db78439d5e02bafaf087b0917bf20
bhushanm@cdac.in <bhushanm@cdac.in>
2025-09-25 10:00:28 +0530

    v1.0

---
+++ src/HelloWorld_New.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld_New.cpp
+// Author      : Bhushan M.
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+//This is version 1
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
